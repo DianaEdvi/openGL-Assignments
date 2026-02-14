8a8a126b8186abc41038c2a9ea88cd83e4118a67
Diana <edvi.diana@gmail.com>
2026-02-14 18:52:19 -0500

    created base class (compiles and runs, yay!)

---
+++ AssignmentTemplates/Assignment1/code/src/A1solution.cpp
@@ -0,0 +1,12 @@
+#include "A1solution.h"
+
+#include <iostream>
+using namespace std;
+
+A1solution::A1solution() {
+    cout << "A1Solution created" << endl;
+}
+
+void A1solution::run(string file_name){
+    cout << "Run run" << endl;
+}
---
+++ AssignmentTemplates/Assignment1/code/src/A1solution.h
@@ -0,0 +1,17 @@
+#ifndef  A1SOLUTION_H
+#define A1SOLUTION_H
+
+#include <iostream>
+
+using namespace std;
+
+class A1solution{
+    public: 
+        A1solution();
+        void run(string file_name);
+    private: 
+        string fileName;
+};
+
+
+#endif
