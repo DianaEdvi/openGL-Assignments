5da79ede14c73d9c8894ef275d6b96d90c2193b3
Diana <edvi.diana@gmail.com>
2026-02-14 22:24:44 -0500

    finished parsing data logic

--- AssignmentTemplates/Assignment1/code/src/Model.cpp
+++ AssignmentTemplates/Assignment1/code/src/Model.cpp
@@ -20,12 +20,42 @@ Model::Model(const std::string& file_name){
         }
     }
 
+    // Read projection matric
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            file >> projection[i][j];
+        }
+    }
+
+    // Read window size 
+    file >> windowWidth;
+    file >> windowHeight;
+
+    // Read vertex data 
+    file >> numberOfVertices;
+    vertices.resize(numberOfVertices);
+
+    for (int i = 0; i < numberOfVertices; i++){
+        file >> vertices[i].x >> vertices[i].y >> vertices[i].z;
+    }
+    
+    // Read index data 
+    file >> numberOfTriangles;
+    indices.resize(numberOfTriangles * 3);
+
+    for (int i = 0; i < numberOfTriangles * 3; i++){
+        file >> indices[i];
+    }
+
 }
 
-std::ostream &operator<<(std::ostream &out, const Model &model){
-    out << "Modelview: \n" << glm::to_string(model.modelView);
-    return out;
+
+// ------------------------ Output streams ------------------------ 
+
+std::ostream& operator<<(std::ostream& os, const glm::vec3& v) {
+    return os << glm::to_string(v);
 }
+
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
@@ -35,4 +65,18 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     }
     os << " ]";
     return os;
+}
+
+
+std::ostream &operator<<(std::ostream &out, const Model &model){
+    out << "Modelview: \n" << glm::to_string(model.modelView) << "\n";
+    out << "Projection: \n" << glm::to_string(model.projection) << "\n";
+    out << "Window width: " << model.windowWidth << "\n";
+    out << "Window height: " << model.windowHeight << "\n";
+    out << "Number of vertices: " << model.numberOfVertices << "\n";
+    out << "Vertices: \n" << model.vertices << "\n";
+    out << "Number of triangles: " << model.numberOfTriangles << "\n";
+    out << "Indices: \n" << model.indices << std::endl;
+
+    return out;
 }
