d39730db8a28ec048468a12020c0acfe315a7d3e
Diana <edvi.diana@gmail.com>
2026-02-14 22:48:46 -0500

    I HAVE A WINDOW YAYYY

--- AssignmentTemplates/Assignment1/code/src/A1solution.cpp
+++ AssignmentTemplates/Assignment1/code/src/A1solution.cpp
@@ -23,6 +23,56 @@ void A1solution::run(std::string file_name){
     std::cout << "Run run" << std::endl;
 
     Model model(file_name);
+    // std::cout << model << std::endl;
 
-    std::cout << model << std::endl;
+    // Initialize GLFW and OpenGL version
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Verify window size
+    if (model.windowWidth <= 0 || model.windowHeight <= 0) {
+        std::cerr << "Error: Invalid window dimensions from model file!" << std::endl;
+        // Fallback so the window at least opens
+        model.windowWidth = 800;
+        model.windowHeight = 600;
+    }
+    // Create Window and rendering context using GLFW, resolution is 800x600
+    GLFWwindow* window = glfwCreateWindow(model.windowWidth, model.windowHeight, "Comp371 - Assignment 1", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to create GLEW" << std::endl;
+        glfwTerminate();
+        return;
+    }
+
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check for events (keyboard, mouse, window close)
+        glfwPollEvents();
+        
+        // Black background
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap the buffers (Display the result)
+        glfwSwapBuffers(window);
+    }
+
+    // Clean up resources when the loop ends
+    glfwTerminate();
+
 }
